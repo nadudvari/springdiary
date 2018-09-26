2effa117ffdaac258b34b3ca5fb43c6f7fb67f66
nadudvari <nadudvari@github.com>
2018-09-26 10:20:56 +0200

    Changed password encryption from sha256 to bcrypt

--- src/main/java/com/example/springdiary/model/Owner.java
+++ src/main/java/com/example/springdiary/model/Owner.java
@@ -11,17 +11,14 @@ public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String name;
+    private String username;
     @OneToOne
     private Diary diary;
     private String password;
-    private byte[] salt;
 
-    public Owner(String name, String password) throws NoSuchAlgorithmException {
-        this.name = name;
-        this.salt = PasswordHash.getSalt();
-        this.password = PasswordHash.get_SHA_256_SecurePassword(password, salt);
-        this.diary = new Diary(name + "\'s Diary");
+    public Owner(String username) {
+        this.username = username;
+        this.diary = new Diary(username + "\'s Diary");
     }
 
     public Owner() {
@@ -31,23 +28,32 @@ public class Owner {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Diary getDiary() {
         return diary;
     }
 
-    public byte[] getSalt() {
-        return salt;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setDiary(Diary diary) {
         this.diary = diary;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
