19d640f094733efd74aceea19bde009da9f221bf
nadudvari <nadudvari@github.com>
2018-09-26 10:18:38 +0200

    Added Bcrypt encoder bean

--- src/main/java/com/example/springdiary/SpringdiaryApplication.java
+++ src/main/java/com/example/springdiary/SpringdiaryApplication.java
@@ -3,6 +3,8 @@ package com.example.springdiary;
 import com.example.springdiary.model.Owner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.security.NoSuchAlgorithmException;
 
@@ -12,5 +14,10 @@ public class SpringdiaryApplication {
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 
 	    SpringApplication.run(SpringdiaryApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder() ;
 	}
 }
