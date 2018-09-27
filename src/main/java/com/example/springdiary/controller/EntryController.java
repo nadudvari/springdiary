d98e888556569c96813556f7684e2de087d0883c
nadudvari <nadudvari@github.com>
2018-09-27 14:18:10 +0200

    Added backend for entry creation

--- src/main/java/com/example/springdiary/controller/EntryController.java
+++ src/main/java/com/example/springdiary/controller/EntryController.java
@@ -1,8 +1,15 @@
 package com.example.springdiary.controller;
 
+import com.example.springdiary.model.Owner;
 import com.example.springdiary.service.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class EntryController {
 
     private EntryService entryService;
@@ -10,5 +17,12 @@ public class EntryController {
     @Autowired
     public EntryController(EntryService entryService) {
         this.entryService = entryService;
+    }
+
+    @PostMapping("/create-entry")
+    public String addEntry(HttpSession session, @ModelAttribute("entry-name") String entryName, @ModelAttribute("entry-content") String entryContent) {
+        Owner owner = (Owner) session.getAttribute("user");
+        entryService.createNewEntry(entryName, entryContent, owner.getDiary());
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/springdiary/service/EntryService.java
+++ src/main/java/com/example/springdiary/service/EntryService.java
@@ -1,5 +1,7 @@
 package com.example.springdiary.service;
 
+import com.example.springdiary.model.Diary;
+import com.example.springdiary.model.Entry;
 import com.example.springdiary.repository.EntryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,11 @@ public class EntryService {
     @Autowired
     public EntryService(EntryRepo entryRepo) {
         this.entryRepo = entryRepo;
+    }
+
+    public void createNewEntry(String entryName, String content, Diary diary) {
+        Entry entry = new Entry(entryName, content, diary);
+        diary.addEntry(entry);
+        entryRepo.save(entry);
     }
 }
